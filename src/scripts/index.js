c36d2301cdded377c00d4bc472040b0d0481cc6c
Apple <apple@Apples-MacBook-Pro.local>
2020-02-19 23:39:41 +0530

    v7

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -34,9 +34,9 @@ function getNews() {
                 //     + 'Reference: ' + urls + '<br>'
                 //     + 'Image: ' + image + '<hr>';
 
-                //patch002 none != none -> style="transform: translateX(50px)"
+                //patch002 none != none -> style="transform: matrix()"
 
-                output +='<li class="article" style="transform: translateX(13px)">'+
+                output +='<li class="article" style="transform: matrix(1, 0.01, 0.01, 0.99, 5, 5);">'+
                     '<div class="card p-1" style="width: 15rem;display:none"><img src="'+image+'" class="article-img" alt="..."></div> '+ //patch005
                     '<div class="card p-1" style="width: 15rem;"><img src="'+image+'" class="card-img-top article-img" alt="..."> '+
                     
@@ -99,7 +99,7 @@ function searchNews() {
             var urls = obj[i].url;
             var image = obj[i].urlToImage;
 
-            output +='<li class="article" style="transform: translateX(13px)">'+
+            output +='<li class="article" style="transform: matrix(1, 0.01, 0.01, 0.99, 5, 5);">'+
                     '<div class="card p-1" style="width: 15rem;display:none"><img src="'+image+'" class="article-img" alt="..."></div> '+ //patch005
                     '<div class="card p-2" style="width: 17rem;"><img src="'+image+'" class="card-img-top article-img" alt="..."> '+
                     
